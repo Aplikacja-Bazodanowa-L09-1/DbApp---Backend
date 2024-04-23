9babd1dd7566c047cab49bd642ed88ca4118e45c
Symonic <93929565+Symonic@users.noreply.github.com>
2024-04-23 16:03:23 +0200

    Created API for password reset (mail confirmation)

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -7,13 +7,19 @@ const jwt = require('jsonwebtoken')
 const LoginController = require('../Controllers/LoginController')
 const TestTokenController = require('../Controllers/TestTokenController')
 const RefreshTokenController = require('../Controllers/RefreshTokenController')
+const RemindPasswordController = require('../Controllers/RemindPasswordController')
+const ResetPasswordController = require('../Controllers/ResetPasswordController')
 
 // MIDDLEWARE
 const authenticateToken = require('../Middleware/authenticateToken')
 
+
+// ROUTES
 Router.get('/test', authenticateToken, TestTokenController)
 Router.post('/token', LoginController)
 Router.post('/refresh_token', RefreshTokenController)
+Router.post('/remind_password', RemindPasswordController)
+Router.post('/reset_password/:token', ResetPasswordController)
 
 
 
